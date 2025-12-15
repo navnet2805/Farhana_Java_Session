7dfda455552a11eb856cb6afeef6e7a871f0dc86
Naveen <naveen.rp.2805@gmail.com>
2025-12-15 22:08:51 +0530

    Commit 34

--- src/Sample.java
+++ src/Sample.java
@@ -5,16 +5,7 @@ import java.util.Set;
 
 public class Sample {
     public static void main(String[] args) {
-       int num1 = 0;
-       int num2 = 1;
-        System.out.print(num1 +" " +num2 + " ");
-       for (int i=1 ; i<=12 ; i++)
-       {
-           int num3 = num1 + num2;
-           num1 = num2;
-           num2 = num3;
-           System.out.print(num3 +" ");
-       }
+
 
 
     }
